1748040c7b9adcbe0de2f80434f5a0257a453d91
Stéphane Chimy <stephane.akira@gmail.com>
2020-08-31 18:55:35 +0200

    Affichage des images en fonction de l'id récupéré dans le localstorage

--- panier.js
+++ panier.js
@@ -1,3 +1,6 @@
+var request = new XMLHttpRequest();
+var allElements;
+var nombreElements;
 var nbOfItems = localStorage.length;
 
 //var itemListParse = JSON.stringify(localStorage);
@@ -5,44 +8,60 @@ var nbOfItems = localStorage.length;
 console.log(localStorage);
 //console.log(itemListParse);
 //console.log(localStorage.length);
-
-if (localStorage.length > 0) {
-
-      for(var nbItem = 0; nbItem < localStorage.length; nbItem++){
-
-        
-        
-        function creatTeddyDiv() {
-            let idProduct = localStorage.getItem("item" + nbItem);
+request.open("GET", "http://localhost:3000/api/teddies");
+request.send();
+request.onload = function () {
+  if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+    allElements = JSON.parse(this.response);
+  }
+  if (localStorage.length > 0) {
+    for (var nbItem = 0; nbItem < localStorage.length; nbItem++) {
+      for (const j of allElements) {
+        var idProduct = localStorage.getItem("item" + nbItem);
+        if (j._id === idProduct) {
+          function creatItem() {
+            var idProduct = localStorage.getItem("item" + nbItem);
             let getProduct = document.getElementById("affPanier");
             let div = document.createElement("div");
             div.setAttribute("id", "item" + nbItem);
             div.className = "card col-xs-12 col-sm-12";
             div.innerHTML =
-              "Vous avez " + localStorage.length + " items dans votre panier, id : " + idProduct;
+              "Vous avez " +
+              localStorage.length +
+              " items dans votre panier, id : " +
+              idProduct;
             getProduct.appendChild(div);
           }
-          creatTeddyDiv();
-    }; 
-       
-    
-  
-} else {
-  function creatTeddyDiv() {
-    let getProduct = document.getElementById("affPanier");
-    let div = document.createElement("div");
-    div.setAttribute("id", "panier");
-    div.className = "card col-xs-12 col-sm-5";
-    getProduct.appendChild(div);
-  }
-  creatTeddyDiv();
+          creatItem();
 
-  function getPanierVide() {
-    const getTeddy = document.getElementById("panier");
-    let p = document.createElement("p");
-    p.className = "card-text";
-    p.innerHTML = "Votre panier est vide";
-    getTeddy.appendChild(p);
+          function getImage() {
+            let getProduct = document.getElementById("item" + nbItem);
+            let img = document.createElement("img");
+            img.setAttribute("src", j.imageUrl);
+            img.className = "image card-img-top img-fluid";
+            getProduct.appendChild(img);
+          }
+          getImage();
+        }
+      }
+    }
+  } else {
+    function creatTeddyDiv() {
+      let getProduct = document.getElementById("affPanier");
+      let div = document.createElement("div");
+      div.setAttribute("id", "panier");
+      div.className = "card col-xs-12 col-sm-5";
+      getProduct.appendChild(div);
+    }
+    creatTeddyDiv();
+
+    function getPanierVide() {
+      const getTeddy = document.getElementById("panier");
+      let p = document.createElement("p");
+      p.className = "card-text";
+      p.innerHTML = "Votre panier est vide";
+      getTeddy.appendChild(p);
+    }
+    getPanierVide();
   }
-  getPanierVide();
-}
+};
