8f5167f3ac470a63a003f74c91eeef5e91cd5115
Stéphane Chimy <stephane.akira@gmail.com>
2020-09-07 14:55:31 +0200

    Affichage nom + quantite

--- panier.js
+++ panier.js
@@ -2,8 +2,7 @@ var request = new XMLHttpRequest();
 var allElements;
 var nombreElements;
 var nbOfItems = localStorage.length;
-
-var panier = [];
+var basket = [];
 
 //var itemListParse = JSON.stringify(localStorage);
 
@@ -15,78 +14,10 @@ request.send();
 request.onload = function () {
   if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
     allElements = JSON.parse(this.response);
-  }
 
-  /* if (localStorage.length > 0) {
-    for (var nbItem = 0; nbItem < localStorage.length; nbItem++) {
-
-      function checkPanier() {
-        //console.log(panier.length);
-        if (panier.length == 0) {
-          var singleItem = [];
-          let nb = 0;
-          singleItem[0] = localStorage.getItem("item" + nbItem);
-          singleItem[1] = nb;
-          panier.push(singleItem);
-        }
-      }
-      
-
-      function addToPanier() {
-
-        for (const iterator in panier) {
-          if (panier[iterator][0] === localStorage.getItem("item" + nbItem)) {
-            //console.log("item deja dans le panier");
-            nb = panier[iterator][1];
-            console.log(nb);
-            nb = nb + 1;
-            panier.splice([iterator][1],1,localStorage.getItem("item" + nbItem),nb);
-          } else {
-            console.log("nouvel item");
-            //singleItem["id"] = localStorage.getItem("item" + nbItem);
-            //singleItem["nb"] = 1;
-            //panier.push(singleItem);
-          }
-        }
-      }
-      checkPanier();
-      addToPanier();
-      //console.log(panier[nbItem]);
-    }
-    console.log(panier); */
-
-    /* for (const j of allElements) {
-            var idProduct = localStorage.getItem("item" + nbItem);
-            var idSearchOnPage = document.getElementById("item" + nbItem);
-            console.log(idSearchOnPage);
-             if (j._id === idProduct && idSearchOnPage === null) {
-              function creatItem() {
-                var idProduct = localStorage.getItem("item" + nbItem);
-                let getProduct = document.getElementById("affPanier");
-                let div = document.createElement("div");
-                div.setAttribute("id", "item" + nbItem);
-                div.className = "card col-xs-12 col-sm-12";
-                div.innerHTML =
-                  "Item : " +
-                  j.name +
-                  " | id : " +
-                  idProduct;
-                getProduct.appendChild(div);
-                
-              }
-              creatItem();
-    
-              function getImage() {
-                let getProduct = document.getElementById("item" + nbItem);
-                let img = document.createElement("img");
-                img.setAttribute("src", j.imageUrl);
-                img.className = "image card-img-top img-fluid";
-                getProduct.appendChild(img);
-              }
-              getImage();
-            }
-          } */
-   else {
+    checkLocalStorage();
+    showBasket();
+  } else {
     function creatTeddyDiv() {
       let getProduct = document.getElementById("affPanier");
       let div = document.createElement("div");
@@ -100,9 +31,80 @@ request.onload = function () {
       const getTeddy = document.getElementById("panier");
       let p = document.createElement("p");
       p.className = "card-text";
-      p.innerHTML = "Votre panier est vide";
+      p.innerHTML =
+        "Un problème sur le serveur est survenu, merci de revenir plus tard";
       getTeddy.appendChild(p);
     }
     getPanierVide();
+  }
+
+  function checkLocalStorage() {
+    if (localStorage.length < 1) {
+      console.log("rien dans le localstorage");
+      console.log("Votre panier est vide");
+      //pushProductInBasket();
+    } else {
+      console.log("Au moins une donnée est dans localStorage");
+      //checkParamFromLocalStorage();
+      //productControlBeforeBasket();
+      getParamFromLocalStorage();
+    }
+  }
+  function getParamFromLocalStorage() {
+    for (nbItem = 0; nbItem < localStorage.length; nbItem++) {
+      basket.push(JSON.parse(localStorage.getItem(localStorage.key(nbItem))));
+      console.log(basket);
+    }
+    //incrementItem();
+  }
+
+  function showBasket() {
+    for (
+      var nbProductInBasket = 0;
+      nbProductInBasket < basket.length;
+      nbProductInBasket++
+    ) {
+      addProductToTable();
+      addProductNameLigneTable();
+      addProductQuantiteLigneTable();
+    }
+    function addProductToTable() {
+      {
+        let getLigneProduct = document.querySelector("tbody");
+        let creatNewLigne = document.createElement("tr");
+        creatNewLigne.setAttribute("id", basket[nbProductInBasket].Nom);
+        getLigneProduct.appendChild(creatNewLigne);
+      }
+    }
+
+    function addProductNameLigneTable() {
+      let getLigneProduct = document.getElementById(
+        basket[nbProductInBasket].Nom
+      );
+      let insertDataOfProduct = document.createElement("td");
+      insertDataOfProduct.setAttribute(
+        "id",
+        "td" + basket[nbProductInBasket].Nom
+      );
+      insertDataOfProduct.innerText = basket[nbProductInBasket].Nom;
+      getLigneProduct.appendChild(insertDataOfProduct);
+    }
+    function addProductQuantiteLigneTable() {
+      let getLigneProduct = document.getElementById(
+        basket[nbProductInBasket].Nom
+      );
+      console.log(getLigneProduct);
+      let insertDataOfProduct = document.createElement("td");
+      insertDataOfProduct.setAttribute(
+        "id",
+        "td" + basket[nbProductInBasket].Quantite
+      );
+      insertDataOfProduct.innerText = basket[nbProductInBasket].Quantite;
+      getLigneProduct.appendChild(insertDataOfProduct);
+    }
+
+    function addProductPriceLigneTable(){
+      
+    }
   }
 };
