d5058c9a342922180c595aad644d562a7aa3fe96
Stéphane Chimy <stephane.akira@gmail.com>
2020-09-14 18:08:51 +0200

    Changement d'ordre pour visibilité

--- confirmation.js
+++ confirmation.js
@@ -1,18 +1,18 @@
-function showConfirmation() {
-  creatDiv()
-}
-
 function getOrderId() {
   return window.location.search.slice(9);
 }
 
 function creatDiv() {
-    let OrderId = getOrderId();
-    let getConfirmation = document.querySelector("#affConfirmation");
-    let div = document.createElement("div");
-    div.className = "card col-8";
-    div.innerHTML = "Votre numéros de commande est le: " + OrderId + ".";
-    getConfirmation.appendChild(div);
-  }
+  let OrderId = getOrderId();
+  let getConfirmation = document.querySelector("#affConfirmation");
+  let div = document.createElement("div");
+  div.className = "card col-8";
+  div.innerHTML = "Votre numéros de commande est le: " + OrderId + ".";
+  getConfirmation.appendChild(div);
+}
 
-  showConfirmation();
+function showConfirmation() {
+  creatDiv();
+}
+
+showConfirmation();
