4b266541fa5a2f327792f4d87a778b366890b04b
Stéphane Chimy <stephane.akira@gmail.com>
2020-09-28 19:33:26 +0200

    rafactor de la fonction de check du localstorage

--- js/intoBasket.js
+++ js/intoBasket.js
@@ -16,33 +16,6 @@ function listenButton() {
   });
 }
 
-// 2 - If localStorage is empty => Put the product in the basket // Else check parameters in localStorage
-// function checkLocalStorage() {
-//   if (localStorage.length < 1) {
-//     console.log("rien dans le localstorage");
-//     pushProductInBasket();
-//   } else {
-//     console.log("Au moins une donnée est dans localStorage");
-//     checkKeyFromLocalStorage();
-//   }
-// }
-// 3 - Check if localStorage have a key named "basket", If yes => checkProduct() // Else pushProductInBasket()
-// function checkKeyFromLocalStorage() {
-//   let keyFound = false;
-//   for (cle = 0; cle < localStorage.length; cle++) {
-//     if (localStorage.key(cle) == "basket") {
-//       console.log("un produit est déjà dans le localstorage");
-//       // Rapatriement des infos de la clé basket dans basket
-//       basket = JSON.parse(localStorage.getItem(localStorage.key(cle)));
-//       console.log(basket);
-//       checkIdProduct(basket);
-//       return (keyFound = true);
-//     }
-//   }
-//   if (!keyFound) {
-//     pushProductInBasket();
-//   }
-// }
 // 4 - Check if the id of the product is in the basket, if yes => incrementItem() // Else pushProductInBasket()
 function checkIdProduct(basket) {
   var productFound = false;
