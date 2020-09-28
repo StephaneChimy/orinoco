363f8bdb77527661b1d9d577d2cdd53b265c2316
Stéphane Chimy <stephane.akira@gmail.com>
2020-09-28 19:32:58 +0200

    Suppression apiRequest.js inutil // refactor de la fonctin de chack du localStorage

--- js/intoBasket.js
+++ js/intoBasket.js
@@ -5,38 +5,44 @@ function listenButton() {
   var getButton = document.querySelector(".addToBasket");
   console.log(getButton);
   getButton.addEventListener("click", () => {
-    checkLocalStorage();
+    if(checkLocalStorageKey("basket")){
+      basket = JSON.parse(localStorage.getItem(localStorage.key(key)));
+      console.log(basket);
+      checkIdProduct(basket);
+    }else{
+      pushProductInBasket();
+    };
     showBadges();
   });
 }
 
 // 2 - If localStorage is empty => Put the product in the basket // Else check parameters in localStorage
-function checkLocalStorage() {
-  if (localStorage.length < 1) {
-    console.log("rien dans le localstorage");
-    pushProductInBasket();
-  } else {
-    console.log("Au moins une donnée est dans localStorage");
-    checkKeyFromLocalStorage();
-  }
-}
+// function checkLocalStorage() {
+//   if (localStorage.length < 1) {
+//     console.log("rien dans le localstorage");
+//     pushProductInBasket();
+//   } else {
+//     console.log("Au moins une donnée est dans localStorage");
+//     checkKeyFromLocalStorage();
+//   }
+// }
 // 3 - Check if localStorage have a key named "basket", If yes => checkProduct() // Else pushProductInBasket()
-function checkKeyFromLocalStorage() {
-  let keyFound = false;
-  for (cle = 0; cle < localStorage.length; cle++) {
-    if (localStorage.key(cle) == "basket") {
-      console.log("un produit est déjà dans le localstorage");
-      // Rapatriement des infos de la clé basket dans basket
-      basket = JSON.parse(localStorage.getItem(localStorage.key(cle)));
-      console.log(basket);
-      checkIdProduct(basket);
-      return (keyFound = true);
-    }
-  }
-  if (!keyFound) {
-    pushProductInBasket();
-  }
-}
+// function checkKeyFromLocalStorage() {
+//   let keyFound = false;
+//   for (cle = 0; cle < localStorage.length; cle++) {
+//     if (localStorage.key(cle) == "basket") {
+//       console.log("un produit est déjà dans le localstorage");
+//       // Rapatriement des infos de la clé basket dans basket
+//       basket = JSON.parse(localStorage.getItem(localStorage.key(cle)));
+//       console.log(basket);
+//       checkIdProduct(basket);
+//       return (keyFound = true);
+//     }
+//   }
+//   if (!keyFound) {
+//     pushProductInBasket();
+//   }
+// }
 // 4 - Check if the id of the product is in the basket, if yes => incrementItem() // Else pushProductInBasket()
 function checkIdProduct(basket) {
   var productFound = false;
