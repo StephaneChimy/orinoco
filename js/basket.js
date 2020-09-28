2f204f18fd9878fdbdb7a457f944f07a5c7564db
Stéphane Chimy <stephane.akira@gmail.com>
2020-09-28 19:10:26 +0200

    refactor de getBasket du à un probleme de variable globale

--- js/basket.js
+++ js/basket.js
@@ -1,64 +1,63 @@
-////////////////////////////////////////////// Variables //////////////////////////////////////////////
 ////////////////////////////////////////////// Fonction //////////////////////////////////////////////
 
-// function checkLocalStorage() {
-//   if (localStorage.length == 0) {
-//     console.log("rien dans le localstorage");
-//     emptyBasket();
-//     return false;
-//   } else {
-//     console.log("Au moins une donnée est dans localStorage");
-//     //getParamFromLocalStorage();
-//     return true;
-//   }
-// }
-
-// function getParamFromLocalStorage() {
-//   var cleFound = false;
-//   for (cle = 0; cle < localStorage.length; cle++) {
-//     if (localStorage.key(cle) == "basket") {
-//       console.log("un item est dans le localstorage");
-//       // Rapatriement des infos de basket
-//       basket = JSON.parse(localStorage.getItem(localStorage.key(cle)));
-//       console.log(basket);
-//       cleFound = true;
-//       return true;
-//     }
-//   }
-//   if (!cleFound) {
-//     emptyBasket();
-//     return false;
-//   }
-// }
-
-
-function showBasket(basket) {
-  for (let product = 0; product < basket.products.length; product++) {
-    addProductToTable(product);
-    addProductNameLigneTable(product,basket);
-    addProductQuantiteLigneTable(product, basket);
-    getPriceOfProduct(product, basket);
-    setPriceOfProduct(product);
-    getTotalPrice();
-  }
-  setTotalPrice(getTotalPrice());
-  setClearButton();
+function getBasket() {
+  let basket = JSON.parse(localStorage.getItem("basket"));
+  return basket;
 }
 
+function showBasket(basket, products) {
+  let totalPrice = 0;
+  // Parcours le basket qui comprend les id des produits
+  for (let index = 0; index < basket.products.length; index++) {
+    //
+    const foundProduct = products.find(
+      (product) => product._id === basket.products[index].id
+    );
+    //
 
+    // Ajoute ligne 0, 1, 2 , 3 ....
+    //addProductToTable(basket.products[index].id);
+    createNode("tbody", "tr", false, "tbody" + index);
+    //
 
+    // Ajoute le nom du produit
+    //addProductNameLigneTable(basket.products[index], basket);
+    createNode(".tbody" + index, "td", false, "tdProduct" + basket.products[index].id, false, false, basket.products[index].name);
+    //
+
+    // Ajoute la quantité
+    //addProductQuantiteLigneTable(basket.products[index], basket);
+    createNode(".tbody" + index, "td", false, "tdQuantite" + basket.products[index].id, false, false, basket.products[index].Quantite);
+    //
+
+    // Ajoute la prix unitaire du produit
+    //setPriceOfProduct(foundProduct.price, basket.products[index].id);
+    createNode(".tbody" + index, "td", false, "tdPrice" + basket.products[index].id, false, false, PriceFormat(foundProduct.price));
+    //
+
+    // Calcul du total
+    totalPrice += foundProduct.price * basket.products[index].Quantite;
+  }
+  // Afficher le total
+  //setTotalPrice(totalPrice);
+  createNode(".total", "th", false, false, false, PriceFormat(totalPrice));
+
+  // Affiche le bouton Clear
+  //setClearButton();
+  createNode(".total", "th", "clear", "btn btn-outline-secondary", false, false, "x");
+}
 
 ////////////////////////////////////////////// Execution of the script //////////////////////////////////////////////
 
 if (checkLocalStorageKey("basket") === true) {
   console.log("Verification passée");
-  getProducts("http://localhost:3000/api", "teddies")
-    .then(() => {
-      showBasket(getBasket());
+  fetchProducts("teddies")
+    .then((products) => {
+      showBasket(getBasket(), products);
     })
     .then(() => {
       clearBasket();
     });
-}else{
+} else {
   emptyBasket();
 }
