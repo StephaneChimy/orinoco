b9115008cb6ee88da06d4db11523112ff0062309
Stéphane Chimy <stephane.akira@gmail.com>
2020-09-29 18:03:07 +0200

    Renommage de fonction pour une meilleure compréhension

--- js/sendOrderToServer.js
+++ js/sendOrderToServer.js
@@ -10,17 +10,17 @@ let order = {
   products: "",
 };
 ////////////////////////////////////////////// Execution of the script //////////////////////////////////////////////
-setProducts();
-setLastnameInput();
-setFirstnameInput();
-setAddressInput();
-setCityInput();
-setEmailInput();
-ClickOnButton();
+setProductsToOrder();
+setLastnameInputToOrderContact();
+setFirstnameInputToOrderContact();
+setAddressInputToOrderContact();
+setCityInputToOrderContact();
+setEmailInputToOrderContact();
+sendOrderToServerByClickOnButton();
 
 ////////////////////////////////////////////// Fonctions //////////////////////////////////////////////
 
-function ClickOnButton() {
+function sendOrderToServerByClickOnButton() {
   let formulaire = document.querySelector("#formulaire");
 
   formulaire.addEventListener("submit", (e) => {
@@ -36,31 +36,31 @@ function ClickOnButton() {
   });
 }
 // Set informations from user in order.contact
-function setFirstnameInput() {
+function setFirstnameInputToOrderContact() {
   let firstName = document.getElementById("inputSurname");
   firstName.addEventListener("input", function (e) {
     order.contact.firstName = e.target.value;
   });
 }
-function setLastnameInput() {
+function setLastnameInputToOrderContact() {
   let lastName = document.getElementById("inputName");
   lastName.addEventListener("input", function (e) {
     order.contact.lastName = e.target.value;
   });
 }
-function setAddressInput() {
+function setAddressInputToOrderContact() {
   let address = document.getElementById("inputAddress");
   address.addEventListener("input", function (e) {
     order.contact.address = e.target.value;
   });
 }
-function setCityInput() {
+function setCityInputToOrderContact() {
   let city = document.getElementById("inputCity");
   city.addEventListener("input", function (e) {
     order.contact.city = e.target.value;
   });
 }
-function setEmailInput() {
+function setEmailInputToOrderContact() {
   let email = document.getElementById("inputEmail");
   email.addEventListener("input", function (e) {
     order.contact.email = e.target.value;
@@ -68,7 +68,7 @@ function setEmailInput() {
   });
 }
 // Set products in order.products
-function setProducts() {
+function setProductsToOrder() {
   let products = [];
   if (checkLocalStorageKey("basket")) {
     let productsInLocalStorage = getLocalstorageKey("basket");
