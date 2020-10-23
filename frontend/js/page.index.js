b1d3cdcecd3f171a75ebf2e9588554e742dfafe3
Stéphane Chimy <stephane.akira@gmail.com>
2020-10-23 11:28:53 +0200

    Changement de chemin à cause de l'ajout du backend

--- frontend/js/page.index.js
+++ frontend/js/page.index.js
@@ -25,7 +25,7 @@ function showProducts(products) {
     createNode(".card-body" + productIndex, "h5", {className : "card-title", innerText : products[productIndex].name});
     createNode(".card-body" + productIndex, "p", {className : "card-text", innerText : products[productIndex].description});
     createNode(".card-body" + productIndex, "p", { className :"card-text", innerText : PriceFormat(products[productIndex].price)});
-    createNode(".card-body" + productIndex, "a", {className : "btn btn-primary", href : "/product.html?ProductId=" + products[productIndex]._id, innerText : "Détail"});
+    createNode(".card-body" + productIndex, "a", {className : "btn btn-primary", href : "/frontend/product.html?ProductId=" + products[productIndex]._id, innerText : "Détail"});
   }
 }
 
